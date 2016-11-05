d4774dd14607978745ce241d0faa0e2f01051a9b
Ivan <optional303@gmail.com>
2016-11-05 20:57:30 +0200

    Intellisense for the project

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/express-serve-static-core/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/serve-static/index.d.ts" />
