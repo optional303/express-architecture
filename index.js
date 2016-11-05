a8f889d313f72bb2e056cea45986d28dbdf48b3f
Ivan <optional303@gmail.com>
2016-11-05 22:38:46 +0200

    Index page and dependencies for pug

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+let express = require('express')
+let mongoose = require('mongoose')
 
+let app = express()
+
+let env = process.env.NODE_ENV || 'development'
+let port = process.env.port || 1337
+mongoose.Promise = global.Promise
+
+app.set('view engine', 'pug')
+app.set('views', './server/views')
+
+app.get('/', (req, res) => {
+    "use strict";
+    mongoose.connect('mongodb://localhost:27017/some-express-db')
+
+    console.log('MongoDB ready!')
+    res.render('index')
+})
+app.listen(port)
+console.log('Server running on port 1337........')
